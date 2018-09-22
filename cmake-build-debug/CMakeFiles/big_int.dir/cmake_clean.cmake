c9355577cf4602a683d05e8723930a514c96d606
yaroslavGrishchuk <37193382+yaroslavGrishchuk@users.noreply.github.com>
2018-09-22 14:41:13 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/big_int.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/big_int.dir/big_integer.cpp.o"
+  "big_int.pdb"
+  "big_int"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/big_int.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/big_integer_testing.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yaroslav/CLionProjects/big_int/big_integer.cpp" "/home/yaroslav/CLionProjects/big_int/cmake-build-debug/CMakeFiles/big_integer_testing.dir/big_integer.cpp.o"
+  "/home/yaroslav/CLionProjects/big_int/big_integer_testing.cpp" "/home/yaroslav/CLionProjects/big_int/cmake-build-debug/CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.o"
+  "/home/yaroslav/CLionProjects/big_int/gtest/gtest-all.cc" "/home/yaroslav/CLionProjects/big_int/cmake-build-debug/CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.o"
+  "/home/yaroslav/CLionProjects/big_int/gtest/gtest_main.cc" "/home/yaroslav/CLionProjects/big_int/cmake-build-debug/CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.o"
+  "/home/yaroslav/CLionProjects/big_int/spare_vector.cpp" "/home/yaroslav/CLionProjects/big_int/cmake-build-debug/CMakeFiles/big_integer_testing.dir/spare_vector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
